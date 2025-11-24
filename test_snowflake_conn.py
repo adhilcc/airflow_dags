ad3719115cc66f5ebc425a8d929b3c035cacadeb
adhilcc <aahammed@ecloudcontrol.com>
2025-11-24 07:18:58 +0000

    test

--- test_snowflake_conn.py
+++ test_snowflake_conn.py
@@ -3,20 +3,21 @@ from airflow.operators.python import PythonOperator
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 from datetime import datetime
 
-def test_key(**_):
+def test_sf(**kwargs):
     hook = SnowflakeHook(snowflake_conn_id="TEST_SF")
-    conn_params = hook._get_conn_params   # <-- critical
-    print("Connection params successfully loaded:")
-    print(conn_params)
+    conn = hook.get_conn()    # <-- ERROR happens here in 2.11
+    cursor = conn.cursor()
+    cursor.execute("SELECT 1")
+    print(cursor.fetchall())
 
 with DAG(
-    dag_id="test_snowflake_key_only",
-    start_date=datetime(2023,1,1),
+    dag_id="test_snowflake_conn",
+    start_date=datetime(2023, 1, 1),
     schedule=None,
     catchup=False,
 ) as dag:
 
     PythonOperator(
         task_id="run_test",
-        python_callable=test_key
+        python_callable=test_sf
     )
