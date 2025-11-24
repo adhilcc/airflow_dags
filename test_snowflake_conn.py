8b8978a2424211de1bcad3254c7db86040e7fb29
adhilcc <aahammed@ecloudcontrol.com>
2025-11-24 08:51:19 +0000

    test

--- test_snowflake_conn.py
+++ test_snowflake_conn.py
@@ -5,16 +5,17 @@ from datetime import datetime
 
 def test_sf(**kwargs):
     hook = SnowflakeHook(snowflake_conn_id="TEST_SF")
-    params = hook._get_conn_params() 
-    print(params)
+
+    # MATCH EXACTLY WHAT YOUR REAL DAG DOES
+    conn = hook.get_conn()   # <---- This will fail in 2.11 with "Incorrect padding"
+    print("Snowflake connection established:", conn)
 
 with DAG(
-    "test_snowflake_conn",
+    dag_id="test_snowflake_conn",
     start_date=datetime(2023, 1, 1),
     schedule=None,
     catchup=False
 ) as dag:
-
     PythonOperator(
         task_id="run_test",
         python_callable=test_sf
